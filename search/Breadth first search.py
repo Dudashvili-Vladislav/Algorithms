e786e5daa4a8976940eb6b2f0bc7dec746d18ec6
Vladislav <77484680+Dudashvili-Vladislav@users.noreply.github.com>
2021-04-08 15:19:01 +0300

    Update Breadth first search.py

--- search/Breadth first search.py
+++ search/Breadth first search.py
@@ -37,3 +37,34 @@ def search(name):
               search_queue += graph[person] # Данные отмечаются как проверенные 
               searched.append(person) #Данные добавляются в массив searched
   return False
+
+#-------------------------------------------------
+# Способ вернуть путь до искомого графа 
+#-------------------------------------------------
+
+def person_is_seller(name): #Функция для критерия поиска
+    return name[-1] == "m"
+
+
+def search(name):
+    search_queue = deque() #Создаем очередь для проверки с помощью функции duque
+    search_queue.append((name, None)) #Добавляем все графы в очередь для поиска 
+    searched = {}  #Используем словарь за место массива
+
+    while search_queue: #Цикл работает пока очередь не будет пуста
+        person, prev = search_queue.popleft() 
+        if person not in searched:
+            searched[person] = prev
+            print(f'Searching {person}') #Смотрим путь до искомого элемента
+        if person_is_seller(person):
+            result = []
+            while person is not None:
+                result.append(person)
+                person = searched[person]
+            return result[::-1]
+        else:
+            search_queue += [(neighbor, person) for neighbor in graph[person]]
+    return []
+
+
+
